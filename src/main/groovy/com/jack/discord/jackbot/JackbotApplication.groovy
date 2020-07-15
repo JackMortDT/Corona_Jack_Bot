5100ee812086725c14b2da0e6a6bf0a6d3e38400
Luisito <le.sastre@ebc.edu.mx>
2020-07-15 17:18:14 -0500

    Start jack bot project

---
+++ src/main/groovy/com/jack/discord/jackbot/JackbotApplication.groovy
@@ -0,0 +1,13 @@
+package com.jack.discord.jackbot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JackbotApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(JackbotApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/jack/discord/jackbot/JackbotApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.jack.discord.jackbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JackbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
