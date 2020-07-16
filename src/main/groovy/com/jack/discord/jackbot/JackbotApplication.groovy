cda31cfacab0a96d21a484a8a9e4bc775c08f474
Luisito <le.sastre@ebc.edu.mx>
2020-07-15 21:27:28 -0500

    Add JDA dependency

--- src/main/groovy/com/jack/discord/jackbot/JackbotApplication.groovy
+++ src/main/groovy/com/jack/discord/jackbot/JackbotApplication.groovy
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class JackbotApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(JackbotApplication, args)
-	}
+  static void main(String[] args) {
+    SpringApplication.run(JackbotApplication, args)
+  }
 
 }
